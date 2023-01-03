b2ce32cf963f63fff1b812283cf20f0d5dc0e8b6
philipthedeveloper <philipowolabi79@gmail.com>
2023-01-03 04:01:44 +0100

    Adding file upload feature

--- app.js
+++ app.js
@@ -3,8 +3,12 @@ const formArray = Array.from(forms);
 const buttons = document.querySelectorAll("button[type=submit]");
 const headerButton = document.querySelectorAll("header > button");
 const navButtons = document.querySelectorAll(".nav-btn-cont button");
+const fileInputs = document.querySelectorAll(".file-upload-container input");
+const uploadButtons = document.querySelectorAll(
+  ".file-upload-container button"
+);
 let currentForm;
-let cValue;
+let cValue = 0;
 
 buttons.forEach((button) => {
   button.addEventListener("click", function (e) {
@@ -16,12 +20,13 @@ buttons.forEach((button) => {
 });
 
 function changeHeaderStyle(value) {
-  console.log("Called with: ", value);
   cValue = value;
   headerButton.forEach((button) => button.classList.remove("active"));
   headerButton[value].classList.add("active");
   if (value < 1) navButtons[0].style.display = "none";
   else navButtons[0].style.display = "block";
+  if (value >= 2) navButtons[1].style.display = "none";
+  else navButtons[1].style.display = "block";
 }
 
 changeHeaderStyle(0);
@@ -48,3 +53,27 @@ function changeForm(val) {
   val.style.display = "flex";
   val.classList.add("current-form");
 }
+
+uploadButtons.forEach((button) => {
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+    let id = button.id;
+    let fileInput = document.querySelector(`input#${id}`);
+    fileInput.click();
+  });
+});
+
+let reader = new FileReader();
+fileInputs.forEach((fileInput) => {
+  fileInput.addEventListener("change", function (e) {
+    let parentImgArea = fileInput.closest(".img-area");
+    let img = parentImgArea.querySelector("img");
+    img.style.display = "block";
+    let image = this.files[0];
+    reader.readAsDataURL(image);
+    reader.onload = (e) => {
+      img.src = reader.result;
+      parentImgArea.append(img);
+    };
+  });
+});
