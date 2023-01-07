f319654e8387dae7105b886d2604a426148ee098
philipthedeveloper <philipowolabi79@gmail.com>
2023-01-07 17:28:35 +0100

    Add event listener to inputs and buttons

--- app.js
+++ app.js
@@ -1,21 +1,96 @@
 const forms = document.forms;
 const formArray = Array.from(forms);
-const buttons = document.querySelectorAll("button[type=submit]");
+const buttons = document.querySelectorAll(
+  "button[type=submit]:not(#submit-all-info)"
+);
 const headerButton = document.querySelectorAll("header > button");
 const navButtons = document.querySelectorAll(".nav-btn-cont button");
 const fileInputs = document.querySelectorAll(".file-upload-container input");
 const uploadButtons = document.querySelectorAll(
   ".file-upload-container button"
 );
-let currentForm;
+const submitButton = document.querySelector("#submit-all-info");
+const submitPersonalInfoBtn = document.querySelector("#submit-a");
+const submitIdentityIdBtn = document.querySelector("#submit-b");
+const personalInfoInputs = document.forms["form-a"].querySelectorAll("input");
+const identityIdInputs = document.forms["form-b"].querySelectorAll("input");
+const idVerificationInputs = document.forms["form-c"].querySelectorAll("input");
+console.log(idVerificationInputs);
 let cValue = 0;
 
-buttons.forEach((button) => {
-  button.addEventListener("click", function (e) {
-    e.preventDefault();
-    let nextForm = button.closest("form").nextElementSibling;
-    currentForm = nextForm;
-    changeForm(nextForm);
+const personalInfo = {
+  firstname: "",
+  lastname: "",
+  address: "",
+  "date-of-birth": "",
+  SSN: "",
+  email: "",
+  "phone-number": "",
+};
+
+const identityID = {
+  username: "",
+  password: "",
+};
+
+const idVerification = {
+  id_card: "",
+  ssd: "",
+  utility_bill: "",
+};
+
+submitButton.addEventListener("click", function (e) {
+  e.preventDefault();
+  for (let key in idVerification) {
+    if (idVerification[key] === "") {
+      alert("Please upload all image");
+      return;
+    }
+  }
+  console.log(idVerification);
+});
+
+submitPersonalInfoBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  for (let key in personalInfo) {
+    if (personalInfo[key] === "") {
+      alert("Please fill all input field");
+      return;
+    }
+  }
+  console.log(personalInfo);
+  changeHeaderStyle(1);
+  newFunction(e.target);
+});
+
+submitIdentityIdBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  for (let key in identityID) {
+    if (identityID[key] === "") {
+      alert("Please fill all input field");
+      return;
+    }
+  }
+  console.log(identityID);
+  changeHeaderStyle(2);
+  newFunction(e.target);
+});
+
+personalInfoInputs.forEach((input) => {
+  input.addEventListener("change", function (e) {
+    personalInfo[e.target.name] = e.target.value;
+  });
+});
+
+function newFunction(button) {
+  let nextForm = button.closest("form").nextElementSibling;
+  currentForm = nextForm;
+  changeForm(nextForm);
+}
+
+identityIdInputs.forEach((input) => {
+  input.addEventListener("change", function (e) {
+    identityID[e.target.name] = e.target.value;
   });
 });
 
@@ -71,8 +146,10 @@ fileInputs.forEach((fileInput) => {
     img.style.display = "block";
     let image = this.files[0];
     reader.readAsDataURL(image);
-    reader.onload = (e) => {
+    reader.onload = () => {
       img.src = reader.result;
+      console.log(e.target.name);
+      idVerification[e.target.name] = reader.result;
       parentImgArea.append(img);
     };
   });
